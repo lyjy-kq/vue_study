8f96df2534eca393abc754ebe3e435266f448b96
qingyu314 <2069646725@qq.com>
2024-03-05 16:56:29 +0800

    02简单效果

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="app">
         <h1>你好</h1>
+        <Person/>
     </div>
 </template>
 
 <script lang="ts">
+    import Person from './components/Person.vue'
+
     export default{
-        name:'app' //组件名
+        name:'app', //组件名
+        components:{Person} //注册组件
     }
 </script>
 
---
+++ src/components/Person.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name:'Person', //组件名
+        data(){
+            return {
+                name:'张三',
+                age:18,
+                tel:'15356500590'
+            }
+        },
+        methods:{
+            showTel(){
+                alert(this.tel)
+            },
+            changeName(){
+                this.name='Zhang San'
+            },
+            changeAge(){
+                this.age+=1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 5px;
+        background-color: red;
+        box-shadow: 0 0 5px;
+        border-radius: 5px;
+    }
+</style>
